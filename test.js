881dadd8450a0449952fbc6c0dbe334f6d9f4ed1
m90 <frederik.ring@gmail.com>
2014-08-02 08:47:10 +0200

    rename main script, improve README, add test, bump version

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var
-ncs = require('./index.js')
+ncs = require('./ncscolor.js')
 , assert = require('assert');
 
 describe('ncs', function(){
@@ -7,6 +7,7 @@ describe('ncs', function(){
 	it('converts NCS color names to hex values', function(){
 		assert.equal(ncs.hex('NCS S 2080-G20Y'), '#65d636');
 		assert.equal(ncs.hex('NCS 2080-G20Y'), '#65d636');
+		assert.equal(ncs.hex('NCS S 2060-B'), '#399bd6');
 	});
 
 	it('converts NCS color names to rgb values', function(){
